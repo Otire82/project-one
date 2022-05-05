6f78927f6565eef49ff50077dfe427c19201a353
Otire82 <a.jeanne01@sncf.fr>
2022-05-05 15:20:45 +0200

    js formulaire ok

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ form.onsubmit = function (event) {
   
   newTodo.innerHTML = inputTodo.value;
   todolist.appendChild(newTodo);
-  newTodo2.innerHTML = inputTodo.value;
+  newTodo2.innerHTML = inputTodo2.value;
   todolist.appendChild(newTodo2);
   
 
@@ -36,42 +36,3 @@ form.onsubmit = function (event) {
   inputTodo2.value = "";
 };    
 
-
-/*
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', function(){}
-
-      for (let i = 0; i < input.length; i++){        
-        if(input.value === "")
-        {
-        alert('Veuillez remplir les champs manquants'); 
-         }
-      }
-});
-
-function newElement() {
-  let li = document.createElement("li");
-  let inputValue = document.getElementById(".input").value;
-  let t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("Veuillez remplir les champs manquants!");
-  } 
-  else {
-    document.getElementById("myUL").appendChild(li)
-  };
-  document.getElementById(".input").value = "";
-
-  let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }} */
